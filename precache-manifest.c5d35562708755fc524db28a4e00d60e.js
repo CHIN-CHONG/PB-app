f8d0eb462592c712f87f1f16c0bad59ad1e144eb
Irishery <63507053+Irishery@users.noreply.github.com>
2020-07-10 17:18:09 +0300

    Updates

---
+++ precache-manifest.c5d35562708755fc524db28a4e00d60e.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6fb6d23b1348faabb02b0a879d73a4c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a564d388e0a4f48c4aaf",
+    "url": "/static/css/2.a6674286.chunk.css"
+  },
+  {
+    "revision": "8d7cbc03f4fb3600a0c2",
+    "url": "/static/css/main.86f0f986.chunk.css"
+  },
+  {
+    "revision": "a564d388e0a4f48c4aaf",
+    "url": "/static/js/2.2901bb08.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/static/js/2.2901bb08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d7cbc03f4fb3600a0c2",
+    "url": "/static/js/main.89f6553f.chunk.js"
+  },
+  {
+    "revision": "cfa39abcec699655de15",
+    "url": "/static/js/runtime-main.60ed06f9.js"
+  },
+  {
+    "revision": "3e343feeb29a818055f787b7c599a86d",
+    "url": "/static/media/art_card.3e343fee.png"
+  },
+  {
+    "revision": "429827e685856f650825235f75142fe8",
+    "url": "/static/media/click.429827e6.mp3"
+  },
+  {
+    "revision": "c71a21e422e5ba58db0e55607b481c8f",
+    "url": "/static/media/deepnude_card.c71a21e4.png"
+  },
+  {
+    "revision": "c7ba09daa93f6a0d547300cee5400e92",
+    "url": "/static/media/stalker_card.c7ba09da.png"
+  },
+  {
+    "revision": "3f51b69aae988495e375c6a5aad64f3b",
+    "url": "/static/media/stick_card.3f51b69a.png"
+  },
+  {
+    "revision": "74f9efad941085a7ceb3d49aa2a40a07",
+    "url": "/static/media/обложка-кнопка-нажми.74f9efad.svg"
+  }
+]);
